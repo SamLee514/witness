2af9483320290c83850f447e66764180a904002c
Ben Siraphob <bensiraphob@gmail.com>
2021-11-11 01:16:11 -0600

    Add Nix flake

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Witness - VandyHacks Judging Platform";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+    bp.url = "github:serokell/nix-npm-buildpackage";
+  };
+
+  outputs = { self, nixpkgs, utils, bp }:
+    utils.lib.eachDefaultSystem (system:
+      with import nixpkgs { inherit system; overlays = [ bp.overlay ]; }; {
+        defaultPackage = buildYarnPackage { src = ./.; };
+      }
+    );
+}
